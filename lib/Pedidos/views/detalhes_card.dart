0c230d1856d460e4afcd923ddd5b978096e9091b
Paulo Manfredini <paulo.manfredini@iteris.com.br>
2022-02-23 11:17:35 -0300

    detalhes pedido card fixed new layout

--- lib/Pedidos/views/detalhes_card.dart
+++ lib/Pedidos/views/detalhes_card.dart
@@ -20,42 +20,62 @@ class CardDetalhes extends StatelessWidget {
             mainAxisAlignment: MainAxisAlignment.spaceAround,
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Text('Produtos'),
-              Padding(padding: EdgeInsets.all(4)),
+              const Text('Produtos'),
+              const Padding(padding: EdgeInsets.all(4)),
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text('Qtd', style: TextStyle(color: Colors.grey)),
-                      // ListView.builder(
-                      //     shrinkWrap: true,
-                      //     physics: const ScrollPhysics(),
-                      //     itemCount: pedido.orderItems.length,
-                      //     itemBuilder: (context, index) {
-                      //        return Text(pedido.orderItems[0].productId.toString());
-                      Text(pedido.orderItems.first.quantity.toString()),
-                      //     }),
+                    children: const [
+                      Text('Qtd', style: TextStyle(color: Colors.grey))
                     ],
                   ),
                   Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text('Produtos',style: TextStyle(color: Colors.grey)),
-                      Text(pedido.orderItems.first.productId.toString()),
+                    children: const [
+                      Text('Produtos', style: TextStyle(color: Colors.grey))
                     ],
                   ),
                   Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text('Valor Unitário', style: TextStyle(color: Colors.grey)),
-                      Text('R\$ ${pedido.orderItems.first.unitPrice.toString()}'),
+                    children: const [
+                      Text('Valor Unitário',
+                          style: TextStyle(color: Colors.grey))
                     ],
                   ),
+                ],
+              ),
+              Column(
+                children: [
+                  ListView.builder(
+                      shrinkWrap: true,
+                      physics: const ScrollPhysics(),
+                      itemCount: pedido.orderItems.length,
+                      itemBuilder: (context, index) {
+                        return Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Expanded(
+                                flex: 2,
+                                child: Text(
+                                    pedido.orderItems[index].quantity.toString())),
+                            Expanded(
+                                flex: 3,
+                                child: Text(
+                                    pedido.orderItems[index].productId.toString())),
+                            Expanded(
+                                flex: 2,
+                                child: Text(
+                                    'R\$ ${pedido.orderItems[index].unitPrice.toStringAsFixed(2)}'),
+                            ),
+                          ],
+                        );
+                      }),
                 ],
               ),
             ],
